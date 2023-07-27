5c43f95245cbf0919a735e9a548a125385d7b31c
Wale <onafesowale@gmail.com>
2023-07-27 21:10:19 +0100

    modified ath guard to functional auth guard

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,43 +1,21 @@
-// import { CanActivateFn } from '@angular/router';
-
-// export const authGuard: CanActivateFn = (route, state) => {
-
-//   return true;
-// };
-
-
-import { Injectable } from '@angular/core';
-import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  Router,
-  RouterStateSnapshot,
-  UrlTree,
-} from '@angular/router';
-import { map, Observable } from 'rxjs';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { map } from 'rxjs';
 import { AccountService } from 'src/app/account/account.service';
 
-@Injectable({
-  providedIn: 'root',
-})
-export class AuthGuard implements CanActivate {
-  constructor(private accountService: AccountService, private router: Router) {}
+export const authGuard: CanActivateFn = (route, state) => {
 
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ): Observable<boolean> {
-    return this.accountService.currentUser$.pipe(
-      map((auth) => {
-        if (auth) {
-          return true;
-        }
-        this.router.navigate(['account/login'], {
-          queryParams: { returnUrl: state.url },
-        });
-        return false;
-      })
-    );
-  }
-}
+  const accountService = inject(AccountService);
 
+  const router = inject(Router);
+
+  return accountService.currentUser$.pipe(
+    map((auth) => {
+      if(auth){
+        return true;
+      }
+     router.navigate(['/account/login'], {queryParams: {returnUrl : state.url}});
+     return false;
+    })
+  );
+};
