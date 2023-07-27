1cc0e3f4be9996bfef8c0182f374f6cdc72ccd7d
Wale <onafesowale@gmail.com>
2023-07-27 21:10:37 +0100

    modified children routes

--- src/app/section/section-routing.module.ts
+++ src/app/section/section-routing.module.ts
@@ -4,22 +4,22 @@ import { SectionComponent } from './section.component';
 import { FesorQuestionsComponent } from './fesor-questions/fesor-questions.component';
 import { BiblequizzesQuestionsComponent } from './biblequizzes-questions/biblequizzes-questions.component';
 import { RevisionQuestionsComponent } from './revision-questions/revision-questions.component';
-import { AuthGuard } from '../core/guards/auth.guard';
+import { authGuard } from '../core/guards/auth.guard';
+
 
 const routes: Routes = [
+  { path: '', component: SectionComponent },
   {
-    path: '',
-    children: [
-      { path: '', component: SectionComponent },
-      { path: 'fesor', canActivate:[AuthGuard],  component: FesorQuestionsComponent },
-      {
-        path: 'biblequizzes',
-        canActivate:[AuthGuard],
-        component: BiblequizzesQuestionsComponent,
-      },
-      { path: 'saved', component: RevisionQuestionsComponent },
-    ],
+    path: 'fesor',
+    canActivate: [authGuard],
+    component: FesorQuestionsComponent,
   },
+  {
+    path: 'biblequizzes',
+    canActivate: [authGuard],
+    component: BiblequizzesQuestionsComponent,
+  },
+  { path: 'saved', component: RevisionQuestionsComponent },
 ];
 
 @NgModule({
