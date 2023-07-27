5435c6c7b14c2b94903cb87648e76c0ed12a6c57
Wale <onafesowale@gmail.com>
2023-07-27 21:09:41 +0100

    specified pathmatch full

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { QuestionComponent } from './shared/question/question.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'section', loadChildren: ()=> import("./section/section.module").then(mod => mod.SectionModule) },
   {path:'account', loadChildren: ()=> import("./account/account.module").then(mod => mod.AccountModule)},
   {path: 'verse', loadChildren: ()=> import("./verse/verse.module").then(mod => mod.VerseModule)},
-  {path: '**', component: HomeComponent}
+  {path: '**', component: HomeComponent, pathMatch: 'full'}
 ];
 
 @NgModule({
