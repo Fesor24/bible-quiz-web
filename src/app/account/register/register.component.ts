70d0c2101a4f6fd0c9f6976fd8d97058c86c3ece
Wale <onafesowale@gmail.com>
2023-08-13 18:47:06 +0100

    restrict registration

--- src/app/account/register/register.component.ts
+++ src/app/account/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AccountService } from '../account.service';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -11,7 +12,7 @@ import { Router } from '@angular/router';
 export class RegisterComponent implements OnInit {
   registerForm!: FormGroup;
 
-  constructor(private fb: FormBuilder, private accountService: AccountService, private router: Router) {}
+  constructor(private fb: FormBuilder, private accountService: AccountService, private router: Router, private toastrService: ToastrService) {}
 
   ngOnInit(): void {
     this.createRegisterForm();
@@ -32,7 +33,7 @@ export class RegisterComponent implements OnInit {
 
   onSubmit() {
     this.accountService.register(this.registerForm.value).subscribe(() =>{
-      this.router.navigateByUrl('/section');
-    }, error => console.log(error));
+      console.log("User created");
+    }, error => this.toastrService.error("Not permitted to register"));
   }
 }
