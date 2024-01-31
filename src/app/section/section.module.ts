10355fecaf48c4b32a0079ac8ef87807e915ac10
Wale <onafesowale@gmail.com>
2024-01-31 08:36:05 +0100

    component modifications

--- src/app/section/section.module.ts
+++ src/app/section/section.module.ts
@@ -1,27 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { SectionRoutingModule } from './section-routing.module';
 import { SectionComponent } from './section.component';
-import { FesorQuestionsComponent } from './fesor-questions/fesor-questions.component';
+import { AuthorQuestionsComponent } from './author-questions/author-questions.component';
 import { RevisionQuestionsComponent } from './revision-questions/revision-questions.component';
 import { BiblequizzesQuestionsComponent } from './biblequizzes-questions/biblequizzes-questions.component';
 import { CoreModule } from '../core/core.module';
 import { SharedModule } from '../shared/shared.module';
 
-
 @NgModule({
   declarations: [
     SectionComponent,
-    FesorQuestionsComponent,
+    AuthorQuestionsComponent,
     RevisionQuestionsComponent,
-    BiblequizzesQuestionsComponent
+    BiblequizzesQuestionsComponent,
   ],
-  imports: [
-    CommonModule,
-    SectionRoutingModule,
-    CoreModule,
-    SharedModule
-  ]
+  imports: [CommonModule, SectionRoutingModule, CoreModule, SharedModule],
 })
-export class SectionModule { }
+export class SectionModule {}
