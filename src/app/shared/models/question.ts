d1c449db672550c9b04fec43a453fa6bc69903fe
Wale <onafesowale@gmail.com>
2024-01-31 08:37:22 +0100

    modified question model

--- src/app/shared/models/question.ts
+++ src/app/shared/models/question.ts
@@ -2,9 +2,17 @@ export interface IQuestion{
   id: number;
   question: string;
   answer: string;
+  verse: string;
+  source: string;
+  passage: string;
 }
 
 export interface ISaveQuestion{
   question: string;
   answer: string;
+}
+
+export interface IQuestionScripture{
+  questionId: number;
+  passage: string;
 }
