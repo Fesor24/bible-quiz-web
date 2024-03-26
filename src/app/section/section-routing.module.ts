6200faf5a6b70b3a330dd6d58143b7ed9d804025
Wale <onafesowale@gmail.com>
2024-03-26 04:23:44 +0100

    added route

--- src/app/section/section-routing.module.ts
+++ src/app/section/section-routing.module.ts
@@ -5,6 +5,7 @@ import { AuthorQuestionsComponent } from './author-questions/author-questions.co
 import { BiblequizzesQuestionsComponent } from './biblequizzes-questions/biblequizzes-questions.component';
 import { RevisionQuestionsComponent } from './revision-questions/revision-questions.component';
 import { authGuard } from '../core/guards/auth.guard';
+import { ObjectiveQuestionsComponent } from './objective-questions/objective-questions.component';
 
 const routes: Routes = [
   { path: '', component: SectionComponent },
@@ -18,6 +19,7 @@ const routes: Routes = [
     // canActivate: [authGuard],
     component: BiblequizzesQuestionsComponent,
   },
+  {path: 'objectives', component: ObjectiveQuestionsComponent},
   { path: 'saved', component: RevisionQuestionsComponent },
 ];
 
